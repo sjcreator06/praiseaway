48dbe40ad9a6663b616e9a901949b2dd74f70972
Samuel Joseph <73075829+sjcreator06@users.noreply.github.com>
2024-05-26 21:46:20 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -451,6 +451,17 @@ function openLyricsSite(title, artist) {
     const xhr = new XMLHttpRequest();
     xhr.open("POST", "/receive_data", true);
 
+    xhr.setRequestHeader("Content-Type", "application/json"); // Set Content-Type header to application/json
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {                                                               
+            if (xhr.status === 200) {
+                const response = JSON.parse(xhr.responseText);
+                window.location.href = "/lyrics?lyrics=" + encodeURIComponent(response.lyrics);
+            } else {
+                console.error("Failed to send data to Flask! Status:", xhr.status);
+            }
+        }
+    };
     
 }
 
